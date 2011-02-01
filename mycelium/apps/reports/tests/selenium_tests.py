e42256e562ad2748c357280aaceddbeea3f810f7
Steven Skoczen <steven@quantumimagery.com>
2011-01-31 21:48:49 -0800

    tweaked reports selenium test to pass

--- mycelium/apps/reports/tests/selenium_tests.py
+++ mycelium/apps/reports/tests/selenium_tests.py
@@ -9,7 +9,7 @@ class TestSelenium(SeleniumTestCase):
         sel.wait_for_page_to_load("30000")
         sel.click("link=Thursday Volunteer List")
         sel.wait_for_page_to_load("30000")
-        try: self.assertEqual("Unsaved Report", sel.get_text("//div[@id='page']/div[2]"))
+        try: self.assertEqual("Thursday Volunteer List", sel.get_text("//div[@id='page']/div[2]"))
         except AssertionError, e: self.verificationErrors.append(str(e))
         try: self.assertEqual("Add new criteria", sel.get_text("link=Add new criteria"))
         except AssertionError, e: self.verificationErrors.append(str(e))
