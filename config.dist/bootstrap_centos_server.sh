7677876228e9f5c371843a04136ed82ea385f254
Steven Skoczen <steven@quantumimagery.com>
2011-02-15 17:35:38 -0800

    updates to have more accurate celery and bootstrapping

--- config.dist/bootstrap_centos_server.sh
+++ config.dist/bootstrap_centos_server.sh
@@ -5,7 +5,7 @@ yum install python26 python26-setuptools python26-devel python26-devel.x86_64 my
 # eventually memcached will get its own server, but not right now.
 cd /etc/init.d; chkconfig --add memcached
 cd /etc/init.d; service memcached start
-cd /etc/init.d; wget https://github.com/ask/celery/raw/master/contrib/generic-init.d/celeryd --no-check-certificate
+cd /etc/init.d; wget https://github.com/ask/celery/raw/master/contrib/generic-init.d/celeryd --no-check-certificate; chmod +x celeryd
 chkconfig rabbitmq-server on
 service rabbitmq-server start
 rabbitmqctl add_user mycelium 68WXmV6K49r8veczVaUK
@@ -23,9 +23,17 @@ cd /var/www
 git clone http://mycelium.skoczen.webfactional.com/mycelium.git mycelium.git
 mkvirtualenv mycelium
 echo 'cd /var/www/mycelium.git' >> /root/.virtualenvs/mycelium/bin/postactivate
-workon mycelinum
+workon mycelium
 pip install -r requirements.txt 
 echo 'mysql_config = /usr/bin/mysql_config' >> ~/.virtualenvs/mycelium/build/mysql-python/site.cfg
 pip install -r requirements.txt 
+
 # need python manage.py celeryd
+cp /var/www/mycelium.git/mycelium/config.dist/celeryd /etc/default/celeryd
+service celeryd start
+
+cp /var/www/mycelium.git/mycelium/config.dist/nginx.conf /etc/nginx/nginx.conf
+killall -HUP nginx
+
 # and run_gunicorn to be running.
+workon mycelium; python /var/www/mycelium.git/mycelium/manage.py run_gunicorn --settings=envs.live --workers=2 --daemon
