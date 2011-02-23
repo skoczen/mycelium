1ccf889eb5970c7ebc462cb3f8aee675ee0540d6
Steven Skoczen <steven@quantumimagery.com>
2011-02-22 22:41:00 -0800

    fixing comma showing up for no city and state

--- media/js/mycelium/people_person.js
+++ media/js/mycelium/people_person.js
@@ -2,12 +2,14 @@ $(function(){
     $("#basic_info_form").bind("genericFieldForm.toggle_off", intelligently_show_hide_comma);
     $("#basic_info_form").bind("genericFieldForm.toggle_on", function(e){$(".city_state_comma",e.target).show();});
     $("#basic_info_form").genericFieldForm();
+    intelligently_show_hide_comma();
 })
 
-function intelligently_show_hide_comma(target) {
- if ($("#container_id_city .view_field",target).html() != "" && $("#container_id_state .view_field").html() != "") {
-     $(".city_state_comma",target).show();   
- } else {
-     $(".city_state_comma",target).hide();
- }
+function intelligently_show_hide_comma() {
+    var target = $("#basic_info_form");
+    if ($("#container_id_city .view_field",target).html() != "" && $("#container_id_state .view_field").html() != "") {
+        $(".city_state_comma",target).show();   
+    } else {
+        $(".city_state_comma",target).hide();
+    }
 }
