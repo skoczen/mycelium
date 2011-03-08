8646dec0c6f73f0f591813791c089b15fffffcd0
Steven Skoczen <steven@quantumimagery.com>
2011-03-07 21:26:03 -0800

    basic models and views in, unit tests passing

---
+++ mycelium/apps/donors/migrations/0001_initial.py
@@ -0,0 +1,98 @@
+# encoding: utf-8
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        
+        # Adding model 'Donor'
+        db.create_table('donors_donor', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('created_at', self.gf('django.db.models.fields.DateTimeField')(null=True, blank=True)),
+            ('modified_at', self.gf('django.db.models.fields.DateTimeField')(null=True, blank=True)),
+            ('person', self.gf('django.db.models.fields.related.OneToOneField')(to=orm['people.Person'], unique=True)),
+        ))
+        db.send_create_signal('donors', ['Donor'])
+
+        # Adding model 'Donation'
+        db.create_table('donors_donation', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('created_at', self.gf('django.db.models.fields.DateTimeField')(null=True, blank=True)),
+            ('modified_at', self.gf('django.db.models.fields.DateTimeField')(null=True, blank=True)),
+            ('donor', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['donors.Donor'])),
+            ('date', self.gf('django.db.models.fields.DateTimeField')()),
+            ('currency', self.gf('django.db.models.fields.CharField')(default='USD', max_length=255)),
+            ('amount', self.gf('django.db.models.fields.DecimalField')(null=True, max_digits=18, decimal_places=2, blank=True)),
+        ))
+        db.send_create_signal('donors', ['Donation'])
+
+
+    def backwards(self, orm):
+        
+        # Deleting model 'Donor'
+        db.delete_table('donors_donor')
+
+        # Deleting model 'Donation'
+        db.delete_table('donors_donation')
+
+
+    models = {
+        'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        'donors.donation': {
+            'Meta': {'object_name': 'Donation'},
+            'amount': ('django.db.models.fields.DecimalField', [], {'null': 'True', 'max_digits': '18', 'decimal_places': '2', 'blank': 'True'}),
+            'created_at': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'currency': ('django.db.models.fields.CharField', [], {'default': "'USD'", 'max_length': '255'}),
+            'date': ('django.db.models.fields.DateTimeField', [], {}),
+            'donor': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['donors.Donor']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'modified_at': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'})
+        },
+        'donors.donor': {
+            'Meta': {'object_name': 'Donor'},
+            'created_at': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'modified_at': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'person': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['people.Person']", 'unique': 'True'})
+        },
+        'people.person': {
+            'Meta': {'ordering': "('first_name', 'last_name')", 'object_name': 'Person'},
+            'city': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'created_at': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'email': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'line_1': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'line_2': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'modified_at': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'phone_number': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'postal_code': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'qi_simple_searchable_search_field': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'state': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'})
+        },
+        'taggit.tag': {
+            'Meta': {'object_name': 'Tag'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'unique': 'True', 'max_length': '100', 'db_index': 'True'})
+        },
+        'taggit.taggeditem': {
+            'Meta': {'object_name': 'TaggedItem'},
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'taggit_taggeditem_tagged_items'", 'to': "orm['contenttypes.ContentType']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'object_id': ('django.db.models.fields.IntegerField', [], {'db_index': 'True'}),
+            'tag': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'taggit_taggeditem_items'", 'to': "orm['taggit.Tag']"})
+        }
+    }
+
+    complete_apps = ['donors']
--- mycelium/apps/donors/models.py
+++ mycelium/apps/donors/models.py
@@ -5,3 +5,96 @@ from taggit.managers import TaggableManager
 from django.db.models.signals import post_save
 import datetime
 
+
+from django.db import models
+from django.utils.translation import ugettext as _
+from qi_toolkit.models import SimpleSearchableModel, TimestampModelMixin
+from taggit.managers import TaggableManager
+from people.models import Person
+from django.db.models.signals import post_save
+import datetime
+
+class Donor(TimestampModelMixin):
+    """A donor!"""
+    person = models.OneToOneField(Person)
+
+    tags = TaggableManager()
+
+    def __unicode__(self):
+        return "%s" % self.person
+
+    class Meta(object):
+        ordering = ["person"]
+
+    @classmethod
+    def make_each_person_a_donor(cls, sender, instance, created=None, *args, **kwargs):
+        if created:
+            d = Donor.objects.get_or_create(person=instance)[0]
+            d.save()
+    
+    @classmethod
+    def make_donors_for_each_person(cls):
+        [cls.make_each_person_a_donor(Person,p,True) for p in Person.objects.all()]
+
+    @property
+    def donations(self):
+        return self.donation_set.all()
+
+    @property
+    def donations_by_year(self):
+        donations_by_year = []
+        current_year = 0
+        cur_year_dict = {}
+        donations_this_year = 0
+        for donation in self.donations:
+            if donation.date.year != current_year:
+                if cur_year_dict != {}:
+                    cur_year_dict['total_donations'] = donations_this_year
+
+                    donations_by_year.append(cur_year_dict)
+
+                current_year = donation.date.year
+                donations_this_year = 0
+                    
+                cur_year_dict = {
+                    'year':donation.date.year,
+                    'donations':[],
+                }
+
+            cur_year_dict["donations"].append(donation)
+            donations_this_year += donation.amount
+        
+        if cur_year_dict != {}:
+            cur_year_dict['total_donations'] = donations_this_year
+            donations_by_year.append(cur_year_dict)
+        return donations_by_year
+
+
+class Donation(TimestampModelMixin):
+    """A specific donation"""
+    donor = models.ForeignKey(Donor)
+    date = models.DateTimeField()
+    currency = models.CharField(default="USD", max_length=255)
+    amount = models.DecimalField(blank=True, null=True, max_digits=18, decimal_places=2)
+
+    def __unicode__(self):
+        return "%s on %s by %s" % (self.pretty_amount, self.date, self.donor)
+
+    class Meta(object):
+        ordering = ["-date", "amount"]
+
+    @property
+    def pretty_amount(self):
+    	return "$%s" % self.amount
+
+
+# class RecurringDonation(TimestampModelMixin):
+#     """A volunteer, scheduled to work a shift"""
+#     volunteer = models.ForeignKey(Volunteer, related_name="volunteer_shifts")
+#     shift = models.ForeignKey(Shift, blank=True, null=True,)
+#     duration = models.DecimalField(blank=True, null=True, max_digits=6, decimal_places=2, help_text="Length of shift, in hours")
+#     date = models.DateField(default=datetime.date.today,verbose_name="Shift date")
+
+#     # categories = TaggableManager()
+    
+post_save.connect(Donor.make_each_person_a_donor,sender=Person)
