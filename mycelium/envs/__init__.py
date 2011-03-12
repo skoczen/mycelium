8eb49cea2a007b53c7c1c45e9724e4cd8e6393d8
Steven Skoczen <steven@quantumimagery.com>
2011-03-11 18:12:20 -0800

    force explicit settings call

--- mycelium/envs/__init__.py
+++ mycelium/envs/__init__.py
@@ -4,10 +4,7 @@ try:
     if "/Users/skoczen/" == cwd[:15]:
         from dev import *
     else:
-        if cwd.find("staging") != -1:
-            from staging import *
-        else:
-            from live import *
+        raise "Settings not specified"
 except:
     from qi_toolkit.helpers import print_exception
     print "Excepted"
