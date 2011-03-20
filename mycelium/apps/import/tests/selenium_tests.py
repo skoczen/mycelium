47ead622b4136cc4702e511af0d3b3ad399dfda0
Steven Skoczen <steven@quantumimagery.com>
2011-03-19 23:38:36 -0700

    selenium tests pass

--- mycelium/apps/import/tests/selenium_tests.py
+++ mycelium/apps/import/tests/selenium_tests.py
@@ -7,49 +7,43 @@ class TestMockupPages(QiConservativeSeleniumTestCase):
     selenium_fixtures = []
     
 
-
-
-
     def test_mockup_pages_load_and_links_work(self):
         sel = self.selenium        
         sel.open("/reports/report/new")
         sel.click("link=More")
         sel.wait_for_page_to_load("30000")
-        try: self.assertEqual("Data Import", sel.get_text("css=.data_import_btn .button_title"))
-        except AssertionError, e: self.verificationErrors.append(str(e))
+        self.assertEqual("Data Import", sel.get_text("css=.data_import_btn .button_title"))
+        
         sel.click("css=.data_import_btn")
         sel.wait_for_page_to_load("30000")
-        try: self.assertEqual("Data Import History", sel.get_text("//div[@id='page']/page_title"))
-        except AssertionError, e: self.verificationErrors.append(str(e))
-        try: self.assertEqual("Start New Import", sel.get_text("link=Start New Import"))
-        except AssertionError, e: self.verificationErrors.append(str(e))
-        try: self.assertEqual("", sel.get_text("//div[@id='page']/record_list/table/tbody/tr[1]/td[1]/img"))
-        except AssertionError, e: self.verificationErrors.append(str(e))
-        try: self.assertEqual("View Results", sel.get_text("link=View Results"))
-        except AssertionError, e: self.verificationErrors.append(str(e))
-        try: self.assertEqual("Right now", sel.get_text("//div[@id='page']/record_list/table/tbody/tr[1]/td[2]/span"))
-        except AssertionError, e: self.verificationErrors.append(str(e))
+        self.assertEqual("Data Import History", sel.get_text("//div[@id='page']/page_title"))
+        self.assertEqual("Start New Import", sel.get_text("link=Start New Import"))
+  
+        self.assertEqual("View Results", sel.get_text("link=View Results"))
+        
+        self.assertEqual("Right now", sel.get_text("css=.in_progress .right_now"))
+        
         sel.click("link=View Results")
         sel.wait_for_page_to_load("30000")
-        try: self.assertEqual("Data Import #5 on Dec 29, 2010 at 3:55pm", sel.get_text("//div[@id='page']/page_title"))
-        except AssertionError, e: self.verificationErrors.append(str(e))
-        try: self.assertEqual("We found 2,937 people.", sel.get_text("css=.report_summary .total"))
-        except AssertionError, e: self.verificationErrors.append(str(e))
-        try: self.assertEqual("Back to All Data Imports", sel.get_text("link=Back to All Data Imports"))
-        except AssertionError, e: self.verificationErrors.append(str(e))
+        self.assertEqual("Data Import #5 on Dec 29, 2010 at 3:55pm", sel.get_text("//div[@id='page']/page_title"))
+        
+        self.assertEqual("We found 2,937 people.", sel.get_text("css=.report_summary .total"))
+        
+        self.assertEqual("Back to All Data Imports", sel.get_text("link=Back to All Data Imports"))
+        
         sel.click("link=Back to All Data Imports")
         sel.wait_for_page_to_load("30000")
-        try: self.assertEqual("Start New Import", sel.get_text("link=Start New Import"))
-        except AssertionError, e: self.verificationErrors.append(str(e))
+        self.assertEqual("Start New Import", sel.get_text("link=Start New Import"))
+        
         sel.click("link=Start New Import")
         sel.wait_for_page_to_load("30000")
-        try: self.assertEqual("Back to All Data Imports", sel.get_text("link=Back to All Data Imports"))
-        except AssertionError, e: self.verificationErrors.append(str(e))
-        try: self.assertEqual("Start Data Import", sel.get_text("//div[@id='page']/page_title"))
-        except AssertionError, e: self.verificationErrors.append(str(e))
-        try: self.assertEqual("Choose a CSV Spreadsheet.", sel.get_text("css=step[number=1] instruction"))
-        except AssertionError, e: self.verificationErrors.append(str(e))
-        try: self.assertEqual("Start Import", sel.get_text("link=Start Import"))
-        except AssertionError, e: self.verificationErrors.append(str(e))
+        self.assertEqual("Back to All Data Imports", sel.get_text("link=Back to All Data Imports"))
+        
+        self.assertEqual("Start Data Import", sel.get_text("//div[@id='page']/page_title"))
+        
+        self.assertEqual("Choose a CSV Spreadsheet.", sel.get_text("css=step[number=1] instruction"))
+        
+        self.assertEqual("Start Import", sel.get_text("link=Start Import"))
+        
         sel.click("link=Back to All Data Imports")
         sel.wait_for_page_to_load("30000")
