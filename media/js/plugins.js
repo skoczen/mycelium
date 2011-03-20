ed2cb2f71a0614ac677b8957553f654f70b10294
Steven Skoczen <steven@quantumimagery.com>
2011-03-20 15:22:01 -0700

    making google analytics happy

--- media/js/plugins.js
+++ media/js/plugins.js
@@ -11,11 +11,11 @@ window.log = function(){
   }
 };
 
-// catch all document.write() calls
-(function(doc){
-  var write = doc.write;
-  doc.write = function(q){ 
-    log('document.write(): ',arguments); 
-    if (/docwriteregexwhitelist/.test(q)) write.apply(doc,arguments);  
-  };
-})(document);
+// // catch all document.write() calls
+// (function(doc){
+//   var write = doc.write;
+//   doc.write = function(q){ 
+//     log('document.write(): ',arguments); 
+//     if (/docwriteregexwhitelist/.test(q)) write.apply(doc,arguments);  
+//   };
+// })(document);
