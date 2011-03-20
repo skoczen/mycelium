4d92214f4885fc6bf1b218f773866ef502396ecf
Steven Skoczen <steven@quantumimagery.com>
2011-03-19 21:27:42 -0700

    moving other user-generated files to underscores

--- mycelium/apps/logo_maker/models.py
+++ mycelium/apps/logo_maker/models.py
@@ -3,7 +3,7 @@ from django.utils.translation import ugettext as _
 from qi_toolkit.models import TimestampModelMixin
 
 class Logo(TimestampModelMixin):
-    image = models.ImageField(upload_to="logos")
+    image = models.ImageField(upload_to="_logos")
     name = models.CharField(max_length=255)
 
     def __unicode__(self):
