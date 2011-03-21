c73d5ade3f0191fb77bc55d84026d0243de799fb
Steven Skoczen <steven@quantumimagery.com>
2011-03-21 11:41:47 -0700

    trying different blog url

--- mycelium/urls.py
+++ mycelium/urls.py
@@ -19,7 +19,7 @@ urlpatterns = patterns('',
     (r'^import/',       include('import.urls',              app_name="import",          namespace="import")),
     (r'^logo/',         include('logo_maker.urls',          app_name="logo_maker",      namespace="logo_maker")),
     
-    (r'^blog/$', 'django.views.generic.simple.redirect_to', {'url': "http://goodrain.agoodcloud.com"}, 'blog_home'),
+    (r'^blog/$', 'django.views.generic.simple.redirect_to', {'url': "http://goodcloud.posterous.com"}, 'blog_home'),
     (r'^admin/doc/', include('django.contrib.admindocs.urls')),
     (r'^admin/', include(admin.site.urls)),
     (r'^', include('sorl.thumbnail.urls')),
