10673006017ed2d084ab300b77c17b5c425d94cf
Steven Skoczen <steven@quantumimagery.com>
2011-03-27 00:07:15 -0700

    groups mockup largely there

--- mycelium/apps/groups/models.py
+++ mycelium/apps/groups/models.py
@@ -2,10 +2,10 @@ from django.db import models
 from django.utils.translation import ugettext as _
 from qi_toolkit.models import SimpleSearchableModel, TimestampModelMixin
 from django.db.models.signals import post_save
-import datetime
-from taggit.managers import TaggableManager
-from taggit.models import TaggedItemBase
 
+
+from people.models import Person
+from test_factory import Factory
 
 class Group(TimestampModelMixin):
     name = models.CharField(max_length=255, blank=True, null=True, unique=True)
@@ -15,6 +15,11 @@ class Group(TimestampModelMixin):
 
     class Meta(object):
         ordering = ("name",)
+
+    def members(self):
+        if not hasattr(self,"cached_members"):
+            self.cached_members = Person.objects.all().order_by("?")[:Factory.rand_int(10,100)]
+        return self.cached_members
 
 class GroupRule(models.Model):
     group = models.ForeignKey(Group)
--- mycelium/apps/groups/urls.py
+++ mycelium/apps/groups/urls.py
@@ -10,8 +10,5 @@ urlpatterns = parser.patterns('',
     url(r'group/new$',                                               views.new_group,                               name='new_group'),    
     url(r'group/{group_id:digits}$',                                 views.group,                                   name='group'),
     url(r'group/delete$',                                            views.delete_group,                            name='delete_group'),    
-
-    url(r'smart_group/new$',                                         views.new_smart_group,                         name='new_smart_group'),    
-    url(r'smart_group/{smart_group_id:digits}$',                     views.smart_group,                             name='smart_group'),
-    url(r'smart_group/delete$',                                      views.delete_smart_group,                      name='delete_smart_group'),    
+    url(r'group/{group_id:digits}/save-basic$',                      views.save_basic_info,                         name='save_basic_info'),
 )
