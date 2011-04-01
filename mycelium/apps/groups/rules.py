58da65904acb49412e0ca486c477160aeb51f130
Steven Skoczen <steven@quantumimagery.com>
2011-03-31 23:46:24 -0700

    rules comments, min_width for group name

--- mycelium/apps/groups/rules.py
+++ mycelium/apps/groups/rules.py
@@ -4,8 +4,21 @@ class Rule(object):
     operator = ""
     right_side = ""
 
+    left_side_available_operators = ""
+"""
+operators:
+
+is_exactly          filter(__iequals)
+is_not_exactly      exclude(__iequals)
+contains            filter(__icontains)
+does not contain    exclude(__icontains)
+is on               date=
+is before           date__lt=
+is after            date__gt
+
 
 RULE_LEFT_SIDES = [
     ()
 ]
 
+"""
