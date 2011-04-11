6e9747a9d68c2c37d775f5e98d79d7cad458373b
Steven Skoczen <steven@quantumimagery.com>
2011-04-11 01:54:30 -0700

    pulling rulegroupform entirely

--- mycelium/apps/rules/forms.py
+++ mycelium/apps/rules/forms.py
@@ -1,16 +1,16 @@
-from django.forms import ModelForm, NullBooleanSelect
-from rules.models import RuleGroup
+# from django.forms import ModelForm, NullBooleanSelect
+# from rules.models import RuleGroup
 
-class RuleGroupForm(ModelForm):
-    class Meta:
-        model = RuleGroup
-        fields = ("name", )
+# class RuleGroupForm(ModelForm):
+#     class Meta:
+#         model = RuleGroup
+#         fields = ("name", )
 
-    def __init__(self, *args, **kwargs):
-        super(RuleGroupForm, self).__init__(*args,**kwargs)
-        if self.instance.pk:
-            for i in range(self.instance.num_blank_rules,16):
-                self.instance.make_blank_rule()
-            super(RuleGroupForm, self).__init__(*args,**kwargs)
+#     def __init__(self, *args, **kwargs):
+#         super(RuleGroupForm, self).__init__(*args,**kwargs)
+#         if self.instance.pk:
+#             for i in range(self.instance.num_blank_rules,16):
+#                 self.instance.make_blank_rule()
+#             super(RuleGroupForm, self).__init__(*args,**kwargs)
 
-        
+#         
