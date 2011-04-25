0d8fa3ff0f6497c52650a458e9d5e2e71f346c18
Steven Skoczen <steven@quantumimagery.com>
2011-04-24 17:54:03 -0700

    testimonials page

--- mycelium/apps/mycelium_core/templatetags/current_page.py
+++ mycelium/apps/mycelium_core/templatetags/current_page.py
@@ -7,6 +7,6 @@ register = template.Library()
 def current_page_link_and_class(request, url, extra_classes=""):
     reverse_str = reverse(url)
     active_str = ""
-    if request.path == reverse:
-        active_str = "active"
+    if request.path == reverse_str:
+        active_str = "current"
     return "href='%s' class='%s %s'" % (reverse_str, active_str, extra_classes)
