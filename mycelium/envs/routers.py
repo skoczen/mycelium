e52446db3a5831ee3d0307786675ad3721c84102
Steven Skoczen <steven@quantumimagery.com>
2011-05-03 21:01:05 -0700

    just using master for everything

--- mycelium/envs/routers.py
+++ mycelium/envs/routers.py
@@ -5,7 +5,8 @@ class MasterSlaveRouter(object):
 
     def db_for_read(self, model, **hints):
         "Point all read operations to a random slave"
-        return random.choice(['slave','default'])
+        # return random.choice(['slave','default'])
+        return 'default'
 
     def db_for_write(self, model, **hints):
         "Point all write operations to the master"
@@ -13,10 +14,11 @@ class MasterSlaveRouter(object):
 
     def allow_relation(self, obj1, obj2, **hints):
         "Allow any relation between two objects in the db pool"
-        db_list = ('default','slave')
-        if obj1._state.db in db_list and obj2._state.db in db_list:
-            return True
-        return None
+        # db_list = ('default','slave')
+        # if obj1._state.db in db_list and obj2._state.db in db_list:
+        #     return True
+        # return None
+        return True
 
     def allow_syncdb(self, db, model):
         "Explicitly put all models on all databases."
