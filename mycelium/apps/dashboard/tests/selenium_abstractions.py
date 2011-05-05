0f297a215e9db2e3d012e9a32408c574f09c540a
Steven Skoczen <steven@quantumimagery.com>
2011-05-05 00:07:52 -0700

    tests for nickname and login stories

--- mycelium/apps/dashboard/tests/selenium_abstractions.py
+++ mycelium/apps/dashboard/tests/selenium_abstractions.py
@@ -3,4 +3,8 @@ import time
 from test_factory import Factory
 
 class DashboardTestAbstractions(object):
-    pass
+    
+    def get_to_the_dashboard(self):
+        sel = self.selenium
+        self.open("/dashboard")
+        sel.wait_for_page_to_load(30000)
