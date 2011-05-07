9d09ce0516c2e203312df52bcf34f8cf8eed8555
Steven Skoczen <steven@quantumimagery.com>
2011-05-06 17:06:15 -0700

    global enter to search

--- media/js/mycelium/mycelium_top_search.js
+++ media/js/mycelium/mycelium_top_search.js
@@ -9,10 +9,10 @@ $(function(){
 	    results_element: $("fragment[name=global_search_results]")
     });
     
-    $("#global_search").bind("myceliumSearch.return_pressed",open_if_only_result);
+    $("#global_search").bind("myceliumSearch.return_pressed",global_open_if_only_result);
 
 });
-function open_if_only_result(e) {
+function global_open_if_only_result(e) {
 	if ($(".global_results_table.results_found").length == 1 && $(".global_results_table.results_found tr.result_row").length == 1) {
 		document.location = $(".global_results_table.results_found tr.result_row td.name a").attr("href");
 	}
