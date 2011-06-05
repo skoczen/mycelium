42cc70e1440e7280dd3cf186cb03b5206a18bc59
Steven Skoczen <steven@quantumimagery.com>
2011-06-05 15:24:18 -0700

    freezing reqs again

--- config.dist/bootstrap_centos_server.sh
+++ config.dist/bootstrap_centos_server.sh
@@ -6,7 +6,14 @@ wget http://dl.iuscommunity.org/pub/ius/stable/Redhat/5/x86_64/epel-release-1-1.
 wget http://dl.iuscommunity.org/pub/ius/stable/Redhat/5/x86_64/ius-release-1.0-6.ius.el5.noarch.rpm
 rpm -Uvh epel-release-1-1.ius.el5.noarch.rpm ius-release-1.0-6.ius.el5.noarch.rpm
 yum install -y nginx
-yum install -y python26 python26-setuptools python26-devel python26-devel.x86_64 mysql-devel.x86_64 sqlite3 memcached hg libjpeg-devel zlib-devel freetype-devel maatkit
+yum install -y python26 python26-setuptools python26-devel python26-devel.x86_64 mysql-devel.x86_64 sqlite3 memcached hg libjpeg-devel zlib-devel freetype-devel maatkit gcc gcc-c++.x86_64 compat-gcc-34-c++.x86_64 openssl-devel.x86_64 zlib*.x86_64
+wget http://www.python.org/ftp/python/2.7/Python-2.7.tar.bz2
+tar -xvjf Python-2.7.tar.bz2
+cd Python*
+./configure --prefix=/opt/python27
+make
+make install
+
 # eventually memcached will get its own server, but not right now.
 cd /etc/init.d; wget https://github.com/ask/celery/raw/master/contrib/generic-init.d/celeryd --no-check-certificate; chmod +x celeryd
 sed '1d' celeryd > celeryd.tmp
@@ -15,8 +22,8 @@ echo "# description: The Celery start-stop-script" >> celeryd
 cat celeryd.tmp >> celeryd
 rm celeryd.tmp
 easy_install-2.6 pip
-echo 'alias python=python26' >> ~/.bashrc
-echo 'VIRTUALENVWRAPPER_PYTHON=/usr/bin/python26' >> ~/.bashrc
+# echo 'alias python=python26' >> ~/.bashrc
+echo 'VIRTUALENVWRAPPER_PYTHON=/opt/python27/bin/python26' >> ~/.bashrc
 source ~/.bashrc
 mkdir /var/www
 mkdir /var/backups
@@ -29,7 +36,7 @@ cd /var/www
 git clone http://mycelium.skoczen.webfactional.com/mycelium.git mycelium.git
 cd mycelium.git;git checkout live
 cat /var/www/mycelium.git/config.dist/authorized_keys >> ~/.ssh/authorized_keys
-mkvirtualenv --no-site-packages mycelium
+mkvirtualenv --no-site-packages --python=/opt/python27/bin/python mycelium
 echo 'cd /var/www/mycelium.git' >> ~/.virtualenvs/mycelium/bin/postactivate
 workon mycelium
 pip install --upgrade pip 
