8256ff10d67b58abc530806e86fc25d54a102a16
Steven Skoczen <steven@quantumimagery.com>
2011-06-05 21:42:28 -0700

    gunicorn tweak

--- config.dist/bootstrap_ubuntu_mycelium_server.sh
+++ config.dist/bootstrap_ubuntu_mycelium_server.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
-aptitude remove apache2
-aptitude install git python2.7 nginx hg libjpeg62 libjpeg62-dev zlib upstart chkconfig python-setuptools python2.7-dev libmysql++-dev
+aptitude remove -y apache2
+aptitude install -y git python2.7 nginx hg libjpeg62 libjpeg62-dev zlib upstart chkconfig python-setuptools python2.7-dev libmysql++-dev
+
 easy_install-2.7 pip
 echo 'VIRTUALENVWRAPPER_PYTHON=/usr/bin/python2.7' >> ~/.bashrc
 source ~/.bashrc
 mkdir /var/www
 rm -rf /var/www/*
 pip install --upgrade pip virtualenv virtualenvwrapper
+
 cd ~;mkdir .virtualenvs
 chmod +x /root
 echo 'source /usr/local/bin/virtualenvwrapper.sh' >> .bashrc
 source ~/.bashrc
+
 cd /var/www
 git clone http://mycelium.skoczen.webfactional.com/mycelium.git mycelium.git
 cd mycelium.git;git checkout live
@@ -31,7 +34,7 @@ cat celeryd.tmp >> celeryd
 rm celeryd.tmp
 chmod +x celeryd
 
-ln -s /var/www/mycelium.git/config.dist/celeryd /etc/init.d/celeryd;chmod +x /etc/init.d/celeryd
+ln -s /var/www/mycelium.git/config.dist/celeryd /etc/default/celeryd;chmod +x /etc/default/celeryd
 ln -s  /var/www/mycelium.git/config.dist/gunicorn /etc/init.d/mycelium; chmod +x /etc/init.d/mycelium
 mv /etc/nginx/nginx.conf /etc/default/nginx.conf.bak
 ln -s /var/www/mycelium.git/config.dist/nginx.conf /etc/nginx/nginx.conf
@@ -40,9 +43,9 @@ groupadd nginx
 useradd -g nginx -s /usr/sbin/nologin -m -d /home/nginx nginx
 mkdir /usr/local/nginx
 
-chkconfig --add celeryd
-chkconfig --add nginx
-chkconfig --add mycelium
+#chkconfig --add celeryd
+#chkconfig --add nginx
+#chkconfig --add mycelium
 service celeryd start
 service mycelium start
 service nginx start
