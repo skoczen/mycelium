f8996caa38c34f17c7913c0f4fc0d6bac6b4f490
Steven Skoczen <steven@quantumimagery.com>
2011-06-09 22:55:43 -0700

    deleting works now too

---
+++ media/js/mycelium/spreadsheets.js
@@ -0,0 +1,17 @@
+$(function(){
+	$(".spreadsheet_delete_btn").click(delete_spreadsheet);	
+});
+
+
+function delete_spreadsheet(e) {
+    var name = $("#container_id_name .view_field").text();
+    if (name == " ") {
+        name = "Unnamed Spreadsheet";
+    }
+    if (confirm("Are you sure you want to completely delete " + name + " from the database? \n\nDeleting will remove this spreadsheet. It will affect any of the people in the spreadsheet.\n\nIt cannot be undone.\n\nPress OK to delete "+ name +".\nPress Cancel to leave things unchanged.")) {
+        $(window).unbind("unload.genericFieldForm");
+        $("#delete_spreadsheet_form").submit();
+    }
+    return false;
+}
+
