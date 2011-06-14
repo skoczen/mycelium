cfee81ee4db9b0c57c63e1ac49350064a517c263
Steven Skoczen <steven@quantumimagery.com>
2011-06-14 00:40:00 -0700

    three passsing tests.

--- mycelium/apps/spreadsheets/tests/selenium_tests.py
+++ mycelium/apps/spreadsheets/tests/selenium_tests.py
@@ -108,4 +108,109 @@ class TestSpreadsheets(QiConservativeSeleniumTestCase, SpreadsheetTestAbstractio
         sel.wait_for_page_to_load("30000")
         self.assert_on_spreadsheet_search_page()
         assert not sel.is_text_present("My test spreadsheet")
+  
+    def test_changing_the_file_type_of_a_spreadsheet_works(self):
+        sel = self.selenium
+        self.test_creating_a_new_spreadsheet_saves()    
+        sel.click("css=#id_default_filetype_0")
+        time.sleep(4)
+        sel.refresh()
+        sel.wait_for_page_to_load("30000")
+        self.assertEqual(sel.get_text("css=.file_type .file_type_option.selected .option_name"), "CSV")
+        sel.click("css=#id_default_filetype_1")
+        time.sleep(4)
+        sel.refresh()
+        sel.wait_for_page_to_load("30000")
+        self.assertEqual(sel.get_text("css=.file_type .file_type_option.selected .option_name"), "Excel")
+
+        sel.click("css=.start_edit_btn")
+        time.sleep(1)
+        sel.click("css=#id_default_filetype_0")
+        time.sleep(4)
+        sel.refresh()
+        sel.wait_for_page_to_load("30000")
+        self.assertEqual(sel.get_text("css=.file_type .file_type_option.selected .option_name"), "CSV")
+
+    
+    def test_that_selecting_a_group_shows_how_many_members_it_has(self):
+        sel = self.selenium
+        self.test_creating_a_new_spreadsheet_saves()
+        sel.click("css=.start_edit_btn")
+        time.sleep(0.5)
+
+        assert sel.is_element_present("css=select#id_group option:nth(5)")
+        assert not sel.is_element_present("css=select#id_group option:nth(6)")
+
+        sel.select("css=#id_group ", "index=1")
+        time.sleep(5)
+        assert sel.is_element_present("css=#id_group_count_with_label")
+        num_members = sel.get_text("css=#id_group_count_with_label .number")
         
+        # get options in group dropdown
+        sel.click("link=Groups")
+        sel.wait_for_page_to_load("30000")
+
+        # get groups
+        self.assertEqual(num_members,sel.get_text("css=.group_row:nth(1) .num_members"))
+        
+    def test_changing_the_template_type_of_a_spreadsheet_works(self):
+        sel = self.selenium
+        self.test_creating_a_new_spreadsheet_saves()    
+        sel.click("css=.start_edit_btn")
+        time.sleep(0.5)
+
+        sel.click("css=#id_spreadsheet_template_1")
+        time.sleep(4)
+        sel.refresh()
+        sel.wait_for_page_to_load("30000")
+        sel.click("css=.start_edit_btn")
+        time.sleep(0.5)
+        self.assertEqual(sel.get_text("css=#container_id_spreadsheet_template .file_type_option.selected .option_name"), "Full Contact List")
+
+        sel.click("css=#id_spreadsheet_template_2")
+        time.sleep(4)
+        sel.refresh()
+        sel.wait_for_page_to_load("30000")
+        sel.click("css=.start_edit_btn")
+        time.sleep(0.5)
+        self.assertEqual(sel.get_text("css=#container_id_spreadsheet_template .file_type_option.selected .option_name"), "Home Mailing List")
+
+        sel.click("css=#id_spreadsheet_template_3")
+        time.sleep(4)
+        sel.refresh()
+        sel.wait_for_page_to_load("30000")
+        sel.click("css=.start_edit_btn")
+        time.sleep(0.5)
+        self.assertEqual(sel.get_text("css=#container_id_spreadsheet_template .file_type_option.selected .option_name"), "Email List")
+
+        sel.click("css=#id_spreadsheet_template_4")
+        time.sleep(4)
+        sel.refresh()
+        sel.wait_for_page_to_load("30000")
+        sel.click("css=.start_edit_btn")
+        time.sleep(0.5)
+        self.assertEqual(sel.get_text("css=#container_id_spreadsheet_template .file_type_option.selected .option_name"), "Donation List")
+
+        sel.click("css=#id_spreadsheet_template_5")
+        time.sleep(4)
+        sel.refresh()
+        sel.wait_for_page_to_load("30000")
+        sel.click("css=.start_edit_btn")
+        time.sleep(0.5)
+        self.assertEqual(sel.get_text("css=#container_id_spreadsheet_template .file_type_option.selected .option_name"), "Donation Summary")
+
+        sel.click("css=#id_spreadsheet_template_6")
+        time.sleep(4)
+        sel.refresh()
+        sel.wait_for_page_to_load("30000")
+        sel.click("css=.start_edit_btn")
+        time.sleep(0.5)
+        self.assertEqual(sel.get_text("css=#container_id_spreadsheet_template .file_type_option.selected .option_name"), "Volunteer Shifts")
+
+        sel.click("css=#id_spreadsheet_template_7")
+        time.sleep(4)
+        sel.refresh()
+        sel.wait_for_page_to_load("30000")
+        sel.click("css=.start_edit_btn")
+        time.sleep(0.5)
+        self.assertEqual(sel.get_text("css=#container_id_spreadsheet_template .file_type_option.selected .option_name"), "Volunteer Hour Summary")
--- mycelium/apps/spreadsheets/tests/unit_tests.py
+++ mycelium/apps/spreadsheets/tests/unit_tests.py
@@ -1,14 +1,30 @@
+import time
+import datetime
+import unittest
+import os
 from test_factory import Factory
-from nose.tools import istest
+from djangosanetesting.cases import DatabaseTestCase, DestructiveDatabaseTestCase
+from qi_toolkit.selenium_test_case import QiUnitTestMixin
+from django.test import TestCase
+from django.conf import settings
+from groups.models import Group
+from data_import.models import DataImport
+from spreadsheets.spreadsheet import SpreadsheetAbstraction, EXCEL_TYPE, CSV_TYPE
+from data_import.tests.abstractions import GenerateSpreadsheetsMixin, TEST_SPREADSHEET_PATH
+from people.models import Person
 
-# @istest
-# def test_verify_content_block():
-#     name = Factory.rand_str()
-#     content = Factory.rand_text()
-#     block = Factory.content_block(internal_name=name, content=content)
-#     
-#     assert block.internal_name == name
-#     assert block.content == content
-#     assert "%s" % block == "Content Block - %s" % (name)
-# 
-# 
+
+class Dummy(object):
+    pass
+
+class TestViews(TestCase, QiUnitTestMixin, DestructiveDatabaseTestCase, GenerateSpreadsheetsMixin):
+
+    def setUp(self):
+        self.a1 = Factory.create_demo_site("test1", quick=True)
+
+    def test_full_contact_list_spreadsheet(self):
+        # starting place
+        people_info = [self._person_dict(p) for p in Person.objects_by_account(self.a1)]
+
+        from spreadsheets.views import download
+        assert True == "Test finished"
