f4aa5e1974709b10df184d08b49a169c2ec2042e
Steven Skoczen <steven@quantumimagery.com>
2011-06-25 19:00:10 -0700

    acutally put it in the template, cleaned up duplicate

--- mycelium/apps/mycelium_core/context_processors.py
+++ mycelium/apps/mycelium_core/context_processors.py
@@ -2,3 +2,9 @@ from django.conf import settings
 def cdn_media_url(request):
     """ Returns CDN_MEDIA_URL url to context."""
     return { 'CDN_MEDIA_URL': settings.CDN_MEDIA_URL, }
+
+def manual_media_url(request):
+    """ Returns MANUAL_MEDIA_URL url to context."""
+    return { 'MANUAL_MEDIA_URL': settings.MANUAL_MEDIA_URL, }
+
+
