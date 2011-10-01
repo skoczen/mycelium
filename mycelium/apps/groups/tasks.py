5a1b0ace9d419c10ec318082e5d96617ecab0890
Steven Skoczen <steven@quantumimagery.com>
2011-09-30 22:11:55 -0700

    idea for consistently updated search results

--- mycelium/apps/groups/tasks.py
+++ mycelium/apps/groups/tasks.py
@@ -3,6 +3,15 @@ from django.core.cache import cache
 
 @task
 def regnerate_all_rulegroup_search_results_for_account(cls, account):
+    # TODO:
+    # set a cache key before calling this task.
+    # task repeats while the key is set
+    # first thing the task does is clear it.
+
+    # End-behavior - recalculation only happens once per account, per change, and
+    #                if a new recalculation is requested while a previous one is happening
+    #                only one more loop will happen.
+
     for g in cls.objects_by_account(account).all():
         cached_num = cache.get(g.cached_count_key)
         if g.members.count() != cached_num:
