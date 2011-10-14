7196ef97603d2bdb734f9a77b62e1b9e14e9a2ee
Steven Skoczen <steven@quantumimagery.com>
2011-10-14 15:37:25 -0700

    pulled MEDIA_URL in jenkins env

--- mycelium/envs/jenkins.py
+++ mycelium/envs/jenkins.py
@@ -15,14 +15,14 @@ DATABASES = {
 CACHE_BACKEND = 'locmem://'
 
 
-MEDIA_URL = '/media/'
-STATIC_URL = MEDIA_URL
-ADMIN_MEDIA_PREFIX = "%sadmin/" % (MEDIA_URL)
+# MEDIA_URL = '/media/'
+# STATIC_URL = MEDIA_URL
+# ADMIN_MEDIA_PREFIX = "%sadmin/" % (MEDIA_URL)
 FAVICON_URL = "%simages/favicon.png" % STATIC_URL
 
 
 SEND_BROKEN_LINK_EMAILS = False
-INTERNAL_IPS = ('127.0.0.1')
+# INTERNAL_IPS = ('127.0.0.1')
 EMAIL_BACKEND = 'django.core.mail.backends.dummy.EmailBackend'
 GOOGLE_KEY = 'ABQIAAAAHhU2Kv9Iz8Fh-GRXaplHqxRi_j0U6kJrkFvY4-OX2XYmEAa76BQkakI7eN4BbYehPxnhnOMnaAhOPw'
 
