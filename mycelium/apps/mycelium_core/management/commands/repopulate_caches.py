e144cbe0a5bb9fc7790f7a83ab7e51eaef69180f
Steven Skoczen <steven@quantumimagery.com>
2011-10-24 22:18:26 -0700

    added spreadsheets to cache population, added output

--- mycelium/apps/mycelium_core/management/commands/repopulate_caches.py
+++ mycelium/apps/mycelium_core/management/commands/repopulate_caches.py
@@ -4,15 +4,28 @@ from django.core.management import call_command
 import subprocess
 from os.path import abspath, join
 
-from people.models import PeopleSearchProxy
-from organizations.models import OrganizationsSearchProxy
-from groups.models import GroupSearchProxy
+
 
 class Command(BaseCommand):
     help = "Repopulate the search caches."
     __test__ = False
 
     def handle(self, *args, **options):
+        from people.models import PeopleSearchProxy
+        from organizations.models import OrganizationsSearchProxy
+        from groups.models import GroupSearchProxy
+        from spreadsheets.models import SpreadsheetSearchProxy
+
+        print "Repopulating people...",
         PeopleSearchProxy.populate_cache()
+        print "done."
+        print "Repopulating organizations..."
         OrganizationsSearchProxy.populate_cache()
+        print "done."
+        print "Repopulating groups..."
         GroupSearchProxy.populate_cache()
+        print "done."
+        print "Repopulating spreadsheets..."
+        SpreadsheetSearchProxy.populate_cache()
+        print "done."
+        
