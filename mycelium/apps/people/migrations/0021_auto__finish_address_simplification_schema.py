0f0d9d2794d055d6692dd5c9155e381238c92850
Steven Skoczen <steven@quantumimagery.com>
2012-02-21 21:23:48 -0800

    pulled complex migration

--- mycelium/apps/people/migrations/0021_auto__finish_address_simplification_schema.py
+++ mycelium/apps/people/migrations/0021_auto__finish_address_simplification_schema.py
@@ -19,8 +19,8 @@ class Migration(SchemaMigration):
         # Deleting model 'ContactMethodType'
         db.delete_table('people_contactmethodtype')
 
-        from people.models import PeopleAndOrganizationsSearchProxy
-        PeopleAndOrganizationsSearchProxy.populate_cache()
+        # from people.models import PeopleAndOrganizationsSearchProxy
+        # PeopleAndOrganizationsSearchProxy.populate_cache()
 
     def backwards(self, orm):
         # Adding model 'EmailAddress'
